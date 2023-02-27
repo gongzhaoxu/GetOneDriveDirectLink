afba4f40716ccf499cb4ccb2dc9f540bea94bd0b
gongzhaoxu <1016661922@qq.com>
2023-02-27 18:45:23 +0800

    更新布局，完善代码

--- script.js
+++ script.js
@@ -126,18 +126,10 @@ function load_mask_local() //从空白加载设置
 		((parseInt(GM_getValue("new-mask-version"),10) || 1)<curMaskVersion)
 	) //没有掩码数据，初始化默认配置。
 	{
-		addNewMask("巩钊旭专用外链","http://storage.live.com/items/${file.id}:/${file.name}?authkey=AHhUrBxZRl_hwM0");
+		addNewMask("我的外链","http://storage.live.com/items/${file.id}:/${file.name}?authkey=AHhUrBxZRl_hwM0");
 		addNewMask("普通外链","http://storage.live.com/items/${file.id}:/${file.name}");
 		addNewMask("最短链接","http://storage.live.com/items/${file.id}");
-		addNewMask("UBB代码外链图片","[img]http://storage.live.com/items/${file.id}:/${file.name}[/img]");
-		addNewMask("模板字符串基本使用示例","在OneDrive里查看 ${file.name} 的地址是：${file.webUrl}");
-		addNewMask("表达式使用示例","${index+1}号文件的尺寸是：${file.size>1024?Math.round(file.size/1024)+\"K\":file.size}B");
-		addNewMask("自动选择img/mp3 UBB代码","[${file.image?\"img\":(file.audio?\"mp3\":\"file\")}]http://storage.live.com/items/${file.id}:/${file.name}[/${file.image?\"img\":(file.audio?\"mp3\":\"file\")}]");
-		addNewMask("ES6完整文件尺寸换算示例","${index+1}号文件的尺寸是：${(function(size){const bArr = [\"B\",\"KiB\",\"MiB\",\"GiB\",\"TiB\"];for(let idx=0;idx<bArr.length;idx++){if(idx<bArr.length && size/Math.pow(1024,idx+1)>1)continue;else return (size/Math.pow(1024,idx)).toFixed(2) + \" \" + bArr[idx];}})(file.size)}");
-		addNewMask("ES6闭包函数示例1","文件的权限是：${(scope=>{switch(scope){case \"anonymous\":return \"所有人\";case \"users\":return \"仅限指定用户\";default:return \"私有\";}})(file.shared?file.shared.scope:null)}");
-		addNewMask("ES6闭包函数示例2","文件年份：${(createTime=>new Date(createTime).toLocaleString('zh-u-ca-chinese-nu-hanidec',{year:\"numeric\",month:\"long\"}))(file.createdDateTime)}");
-		addNewMask("第三方 1drv.ws 项目","${file.permissions[0].link.webUrl.replace(\"1drv.ms\",\"1drv.ws\")}");
-		addNewMask("官方图片下载直连（短期？）","${file[\"@microsoft.graph.downloadUrl\"].replace(/public\.\w+\.files/i,\"public.ch.files\")}");
+		addNewMask("如何自定义自己的掩码？","将我的外链掩码的authkey修改为你自己的即可");
 		if (Array.isArray(masksCfg))
 		{addNewMask("▲以上为版本更新，重新添加的掩码示例","");}
 		GM_setValue("new-mask-version",curMaskVersion);
